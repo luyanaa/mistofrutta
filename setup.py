adfa0d18bfbe01f5498fbe7079f65fe0679aa00c
Yan Lu <robinluaa@outlook.com>
2023-12-11 14:22:36 +0900

    Fix building

--- setup.py
+++ setup.py
@@ -3,34 +3,29 @@
 from distutils.core import setup, Extension
 from distutils.command.build_ext import build_ext
 import os
+import numpy
 
 class CustomBuildExtCommand(build_ext):
     """build_ext command for use when numpy headers are needed."""
 
     def run(self):
-
-        # Import numpy here, only when headers are needed
-        import numpy
-        # Add numpy headers to include_dirs
-        self.include_dirs.append(numpy.get_include())
-        
         # Call original build_ext command
         build_ext.run(self)
 
 
 approx_c = Extension('mistofrutta.approx._approx_c',
                     sources = ['mistofrutta/approx/_approx_c.cpp'],
-                    include_dirs = [],
+                    include_dirs = [numpy.get_include()],
                     extra_compile_args=['-ffast-math','-Ofast'])
                     
 _ft = Extension('mistofrutta.ft._ft',
                     sources = ['mistofrutta/ft/_ft.cpp','mistofrutta/ft/ft.cpp'],
-                    include_dirs = [],
+                    include_dirs = [numpy.get_include()],
                     extra_compile_args=['-ffast-math','-Ofast'])
                     
 _convolve = Extension('mistofrutta.convolve._convolve',
                     sources = ['mistofrutta/convolve/_convolve.cpp','mistofrutta/convolve/convolve.cpp'],
-                    include_dirs = [],
+                    include_dirs = [numpy.get_include()],
                     extra_compile_args=['-ffast-math','-Ofast'])
 					
 
@@ -42,13 +37,13 @@ if os.name == "nt":
 	_ft.include_dirs.append(numpy.get_include())
 	_convolve.include_dirs.append(numpy.get_include())
 
-import git
+# import git
 # Get git commit info to build version number/tag
-repo = git.Repo('.git')
-git_hash = repo.head.object.hexsha
-git_url = repo.remotes.origin.url
-v = repo.git.describe(always=True)
-if repo.is_dirty(): v += ".dirty"
+# repo = git.Repo('.git')
+# git_hash = repo.head.object.hexsha
+# git_url = repo.remotes.origin.url
+# v = repo.git.describe(always=True)
+# if repo.is_dirty(): v += ".dirty"
 
 requirements = [
     "numpy",
@@ -58,7 +53,7 @@ requirements = [
 
 
 setup(name='mistofrutta',
-      version=v,
+      version="1.0.0",
       description='Collection of random utilities',
       author='Francesco Randi',
       author_email='francesco.randi@gmail.com',
